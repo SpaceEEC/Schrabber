2ffdd0ec8ef90718216facc58c4d929ad07e7079
SpaceEEC <spaceeec@yahoo.com>
2018-09-03 16:50:32 +0200

    refactor(SplitWindow): extract ItemTemplate into UserControl for reuse

---
+++ Schrabber/Controls/PartViewModelControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Schrabber.Controls
+{
+	/// <summary>
+	/// Interaction logic for PartViewModelControl.xaml
+	/// </summary>
+	public partial class PartViewModelControl : UserControl
+	{
+		public PartViewModelControl() => this.InitializeComponent();
+	}
+}
